47f93117851e222059a0f317aa2ca29ffd598d74
Ittani maemo <42994867+ittani@users.noreply.github.com>
2024-02-14 11:18:06 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import  FirstComponent from './Components/Learning-examples/FirstComponent';
+import SecondComponent from './Components/Learning-examples/SecondComponent';
+import ThirdComponent from './Components/Learning-examples/ThirdComponent';
+import FourthComponent from './Components/Learning-examples/FourthComponent';
+import {FifthComponent} from './Components/Learning-examples/FirstComponent';
+import LearningJavaScript from './Components/Learning-examples/LearningJavaScript';
+import Counter from './Components/Counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+
+     {/* <FirstComponent></FirstComponent>
+     <SecondComponent></SecondComponent>
+     <ThirdComponent></ThirdComponent>
+     <FourthComponent></FourthComponent>
+     <FifthComponent></FifthComponent>
+      <LearningJavaScript></LearningJavaScript> */} 
+    {/* <PlayingwithPros prooerty1="value1" prooerty2="value2"  /> */}
+
+   <Counter/>
+  
+    </div>
+  );
+}
+
+// function PlayingwithPros(properties) {
+
+//   return (
+//     <div>Props</div>
+//   )
+// }
+
+export default App;
